ecbedb9e7fc82ae08f6f04a619f7a33b7f34db73
Ollie Cockrum <davisbruce413@gmail.com>
2023-05-19 09:07:19 -0500

    Added boilerplate html & css, added window opening

--- pages/pageHandler.js
+++ pages/pageHandler.js
@@ -4,14 +4,35 @@ const path = require("path");
 class PageHandler {
   constructor() {
     console.log("Page Handler created");
+    this.openWindows = {
+      chapter1: false,
+      chapter2: false,
+      chapter3: false,
+      chapter4: false,
+      chapter5: false,
+      chapter6: false,
+      chapter7: false,
+      chapter8: false,
+      chapter9: false,
+      chapter10: false,
+      chapter11: false,
+      chapter12: false,
+    };
   }
 
   start() {
     console.log("Page Handler started!");
 
     app.whenReady().then(() => {
-      const entranceWindow = this.createWindow("entrance", 800, 600);
-      ipcMain.handle("ping", () => "pong");
+      this.createWindow("entrance", 400, 600);
+      ipcMain.handle("open", (event, name) => {
+        if(this.openWindows[name]) {
+          return console.log("Already opened", name);
+        }
+        console.log("Opening", name);
+        this.openWindows[name] = true;
+        this.createWindow(name, 800, 600);
+      });
     });
 
     app.on("window-all-closed", () => {
